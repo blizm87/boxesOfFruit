0dd4e17a73e6240999658693b3af24ecd0860c20
Justin Samuels <blizm87@gmail.com>
2017-08-27 04:56:09 -0700

    add icons, cards, links, and button, styled front end of application.

--- public/js/app.js
+++ public/js/app.js
@@ -5,21 +5,37 @@
   angular
     .module('BoxesOfFruitApp', ['ngRoute'])
     .config(routes)
-    .controller('mainCtrl', ['$scope', '$http', '$timeout', '$window', mainCtrl])
+    .controller('mainCtrl', ['$scope', '$http', '$timeout', mainCtrl])
+    .controller('indexCtrl', ['$scope', '$http', '$timeout', indexCtrl])
+    .controller('gameCtrl', ['$scope', '$http', '$timeout', gameCtrl])
 
     //  CONTROLLER FUNCTION HANDLER SECTION
 
-    function mainCtrl($scope, $http, $timeout, $window){
+    function mainCtrl($scope, $http, $timeout){
       console.log('I am the main controller')
 
     } //  END MAINCTRL -  CONTROLLER
+
+    function indexCtrl($scope, $http, $timeout){
+      console.log('I am the index controller')
+
+    } //  END INDEXCTRL -  CONTROLLER
+
+    function gameCtrl($scope, $http, $timeout){
+      console.log('I am the game controller')
+
+    } //  END GAMECTRL -  CONTROLLER
 
     //  ANGULAR ROUTE HANDLER SECTION
     function routes($routeProvider){
       $routeProvider
         .when('/', {
           templateUrl: '../partials/index.html',
-          controller: 'mainCtrl'
+          controller: 'indexCtrl'
+        })
+        .when('/active', {
+          templateUrl: '../partials/game.html',
+          controller: 'gameCtrl'
         })
         .otherwise({
           rediretTo: '/',
--- src/routes/members.js
+++ src/routes/members.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
 
 router.get('/', (req, res, next) => {
   res.json({data: 'Members route works'})
