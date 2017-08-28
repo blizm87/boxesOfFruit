1879760c467796e703db9dda057e7419bfceac4b
Justin Samuels <blizm87@gmail.com>
2017-08-28 02:52:49 -0700

    added bucket and fruit logic including drag and drop functionality

--- public/js/app.js
+++ public/js/app.js
@@ -29,10 +29,14 @@
       let instrCont = $('#instructionContent');
       let mixedFruitCont = $('#mixedFruitContainer');
       let mixedFruit = document.createElement('img');
+      let singleFruit = document.querySelectorAll('.fruitSelectors');
+      let bucket = document.querySelectorAll('.bucket');
       let canvas = document.getElementById('mixedFruitCanvas');
       let context = canvas.getContext('2d');
       let img1 = new Image();
       let img2 = new Image();
+      let fruitTypes = ['Apples', 'Oranges', 'Mixed'];
+      let bucketArr = [];
 
       $(document).ready(function(){
         $('.tooltipped').tooltip({delay: 50});
@@ -50,16 +54,82 @@
 
       mixedFruit.src = canvas.toDataURL();
       mixedFruit.width = '135';
-      mixedFruit.classList.add('responsive-img');
+      mixedFruit.draggable = 'true';
+      mixedFruit.setAttribute('id', 'mixedFruit');
+      mixedFruit.setAttribute('class', 'responsive-img');
       mixedFruitCont.append(mixedFruit);
 
+    // FRUITSELECTOR DRAG FEATURE
+      mixedFruit.addEventListener('dragstart', function(ev) {
+        ev.dataTransfer.setData("fruit", ev.target.id);
+      });
+      singleFruit[0].addEventListener('dragstart', function(ev) {
+        ev.dataTransfer.setData("fruit", ev.target.id);
+      });
+      singleFruit[1].addEventListener('dragstart', function(ev) {
+        ev.dataTransfer.setData("fruit", ev.target.id);
+      });
+    // FRUIT SELECTOR DRAG FEATURE END
+
+    // BUCKET DROP FEATURE
+      bucket[0].addEventListener('dragenter', function(ev) {
+        ev.preventDefault();
+      });
+      bucket[0].addEventListener('dragover', function(ev) {
+        ev.preventDefault();
+      });
+      bucket[0].addEventListener('drop', function(ev) {
+        ev.preventDefault();
+        if(ev.target.children[0] === undefined){
+          var data = ev.dataTransfer.getData("fruit");
+          ev.target.appendChild(document.getElementById(data));
+        }
+      });
+
+      bucket[1].addEventListener('dragenter', function(ev) {
+        ev.preventDefault();
+      });
+      bucket[1].addEventListener('dragover', function(ev) {
+        ev.preventDefault();
+      });
+      bucket[1].addEventListener('drop', function(ev) {
+        ev.preventDefault();
+        if(ev.target.children[0] === undefined){
+          var data = ev.dataTransfer.getData("fruit");
+          ev.target.appendChild(document.getElementById(data));
+        }
+      });
+
+      bucket[2].addEventListener('dragenter', function(ev) {
+        ev.preventDefault();
+      });
+      bucket[2].addEventListener('dragover', function(ev) {
+        ev.preventDefault();
+      });
+      bucket[2].addEventListener('drop', function(ev) {
+        ev.preventDefault();
+        if(ev.target.children[0] === undefined){
+          var data = ev.dataTransfer.getData("fruit");
+          ev.target.appendChild(document.getElementById(data));
+        }
+      });
+    // BUCKET DROP FEATURE END
       $http
         .get(`http://127.0.0.1:3000/members?profileId=${paramRes}`)
         .then(function(response){
-          console.log(response)
+          $scope.player = response.data.data;
+          for(var i = 0; i < 3; i++){
+            let randNum = Math.floor(Math.random() * fruitTypes.length)
+            let bucketType = fruitTypes.splice(randNum, 1);
+            bucketArr.push(bucketType[0]);
+          }
+          $scope.bucketOne = bucketArr[0];
+          $scope.bucketTwo = bucketArr[1];
+          $scope.bucketThree = bucketArr[2];
         }, function(err){
           console.log(err)
         })
+
     } //  END GAMECTRL -  CONTROLLER
 
     //  ANGULAR ROUTE HANDLER SECTION
