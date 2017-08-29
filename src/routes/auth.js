9003509b903242fbd5bc14595cf85c9d158c650b
Justin Samuels <blizm87@gmail.com>
2017-08-29 09:59:42 -0700

    yo

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -101,7 +101,7 @@ router.get('/linkedin/callback', (req, res1, next) => {
     var redirect_uri = 'http://127.0.0.1:3000/auth/linkedin/callback';
   }
   const {code, state} = req.query;
-  let url = 'https://www.linkedin.com/oauth/v2/accessToken';
+  let url = 'www.linkedin.com/oauth/v2/accessToken';
   const form = {
     code,
     client_id,
@@ -114,7 +114,7 @@ router.get('/linkedin/callback', (req, res1, next) => {
     console.log('I GET AN ACCESS TOKEN HERE')
     console.log(body)
     const data = JSON.parse(body);
-    url = 'http://www.api.linkedin.com/v1/people/~';
+    url = 'https://.api.linkedin.com/v1/people/~';
     const access_token = data.access_token;
     const options = {
       method: 'GET',
