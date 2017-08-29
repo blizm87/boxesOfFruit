b8cca89c8b97b56e8ab400e59b0469475acce62a
Justin Samuels <blizm87@gmail.com>
2017-08-29 09:48:16 -0700

    fixed typo

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -119,7 +119,7 @@ router.get('/linkedin/callback', (req, res1, next) => {
     const options = {
       method: 'GET',
       url,
-      Connection: 'Keep-Alive'
+      Connection: 'Keep-Alive',
       'Authorization' : `Bearer ${access_token}`
     }
     request(options, (err, response, body2) => {
